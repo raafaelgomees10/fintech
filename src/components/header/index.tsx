bae47236356c9d0deef67cc5503d3f7fa23fb439
Rafael Gomes <raafaelgomees10@gmail.com>
2024-10-17 15:17:28 -0300

    add assets, creating menu content

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import DateRange from "../dateRange";
 import MonthGroup from "../monthGroup";
 
 const Header = () => {
+  const [title, setTitle] = useState("Summary");
   return (
     <header className="mb">
-      <div className="mb">
+      <div className="daterange mb">
         <DateRange />
+        <h1 className="box bg3">{title}</h1>
       </div>
       <MonthGroup />
     </header>
--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,5 +1,55 @@
+import resumo from "../../assets/icons/resumo.svg";
+import vendas from "../../assets/icons/vendas.svg";
+import webhooks from "../../assets/icons/webhooks.svg";
+import configuracoes from "../../assets/icons/configuracoes.svg";
+import contato from "../../assets/icons/contato.svg";
+import sair from "../../assets/icons/sair.svg";
+import FintechSVG from "../../assets/FintechSVG";
+
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <nav className="sidenav box bg3">
+      <FintechSVG title="Fintech Logo" />
+      <ul>
+        <li>
+          <span>
+            <img src={resumo} alt="menu icon" />
+          </span>
+          <a href="">Resumo</a>
+        </li>
+        <li>
+          <span>
+            <img src={vendas} alt="menu icon" />
+          </span>
+          <a href="">Vendas</a>
+        </li>
+        <li>
+          <span>
+            <img src={webhooks} alt="menu icon" />
+          </span>
+          <a>WebHooks</a>
+        </li>
+        <li>
+          <span>
+            <img src={configuracoes} alt="menu icon" />
+          </span>
+          <a>Configurações</a>
+        </li>
+        <li>
+          <span>
+            <img src={contato} alt="menu icon" />
+          </span>
+          <a>Contato</a>
+        </li>
+        <li>
+          <span>
+            <img src={sair} alt="menu icon" />
+          </span>
+          <a>Sair</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Menu;
