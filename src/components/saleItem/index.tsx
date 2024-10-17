f435bbe16c894c99378ad141f38ddc8b4a1a2004
Rafael Gomes <raafaelgomees10@gmail.com>
2024-10-17 15:46:57 -0300

    add content to Sale page, create loading component

--- src/components/saleItem/index.tsx
+++ src/components/saleItem/index.tsx
@@ -1,11 +1,12 @@
+import { NavLink } from "react-router-dom";
 import { IVenda } from "../../context/dataContext";
 
 const SaleItem = ({ sale }: { sale: IVenda }) => {
   return (
     <div className="sale box">
-      <a href="" style={{ fontFamily: "monospace" }}>
+      <NavLink to={`/sales/${sale.id}`} style={{ fontFamily: "monospace" }}>
         {sale.id}
-      </a>
+      </NavLink>
       <div>{sale.nome}</div>
       <div>
         {sale.preco.toLocaleString("pt-BR", {
--- src/pages/Sale.tsx
+++ src/pages/Sale.tsx
@@ -1,5 +1,36 @@
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import { IVenda } from "../context/dataContext";
+import Loading from "../components/loading";
+
+type VendaWithoutData = Omit<IVenda, "data">;
+
 const Sale = () => {
-  return <div>Sale</div>;
+  const { id } = useParams();
+
+  const { data, loading } = useFetch<VendaWithoutData>(
+    `https://data.origamid.dev/vendas/${id}`
+  );
+
+  if (loading) return <Loading />;
+
+  if (data === null) return null;
+
+  return (
+    <div>
+      <div className="box mb">ID: {data.id}</div>
+      <div className="box mb">Nome: {data.nome}</div>
+      <div className="box mb">
+        Preco{" "}
+        {data.preco.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </div>
+      <div className="box mb">Status: {data.status}</div>
+      <div className="box mb">Pagamento: {data.pagamento}</div>
+    </div>
+  );
 };
 
 export default Sale;
